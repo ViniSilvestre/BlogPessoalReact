a4f3a9a40689773879b5b9ad1c033af6a4dfef29
Vinicius Silvestre <vinicius.moreira134@yahoo.com>
2022-07-30 16:21:38 -0300

    Adicionando mais componentes ao React - Blog Pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './paginas/home/Home'
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-
+import Login from './paginas/login/Login'
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+      <Router>
+        <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes> // Antigo Switch
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />            
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
   );
 }
 
