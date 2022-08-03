37d9be5e61c8dfa81ac4f7b47db8d7738fe369e6
Vinicius Silvestre <vinicius.moreira134@yahoo.com>
2022-08-03 15:01:22 -0300

    Adicionando postagens e temas ao meu blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './paginas/home/Home'
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Login from './paginas/login/Login'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+import ListaTema from './components/temas/listaTema/ListaTema';
 
 function App() {
   return (
       <Router>
         <Navbar />
         <div style={{ minHeight: '100vh' }}>
-          <Routes> // Antigo Switch
+          <Routes> 
             <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />            
+            <Route path="/home" element={<Home />} />  
+            <Route path="/login" element={<Login />} />       
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />   
+            <Route path="/temas" element= {<ListaTema />} />
+            <Route path="/posts" element= {<ListaPostagem />} />
           </Routes>
         </div>
         <Footer />
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography, Grid, Paper, Button } from "@material-ui/core";
 import { Box } from "@mui/material";
+import TabPostagem from "../../components/postagens/tabPostagem/TabPostagem";
 
 import './Home.css';
 
@@ -23,6 +24,7 @@ function Home(){
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
